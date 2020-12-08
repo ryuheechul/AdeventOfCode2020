4e6cfa528023248f9d8db59ac0bc15d2111b066e
Heechul Ryu <ryuhcii@gmail.com>
2020-12-08 21:57:27 +0000

    Add day8

---
+++ day8/src/main.rs
@@ -0,0 +1,102 @@
+//TODO: use input file
+
+fn main() {
+  let dum = dummy();
+  let tokens = tokenize(dum);
+
+  match acc_just_before_limbo(vec![], 0, 0, tokens) {
+    Some(acc) => println!("Immediately before the program would run an instruction a second time, the value in the accumulator is {:?}", acc),
+    None => println!("I think pointer reached the end of the instructions or something went wrong."),
+  }
+}
+
+fn acc_just_before_limbo(
+  been_there: Vec<u8>,
+  acc: i16,
+  pointer: isize,
+  insts: Vec<Instruction>,
+) -> Option<i16> {
+  if pointer < 0 {
+    return None;
+  }
+
+  let pointer: usize = pointer as usize;
+
+  let (op, operand) = (&insts[pointer]).values();
+
+  let (a, p): (i16, isize) = match op {
+    Op::Nop => (acc, pointer as isize + 1),
+    Op::Acc => (acc + operand, pointer as isize + 1),
+    Op::Jmp => (acc, pointer as isize + operand as isize),
+  };
+
+  if p >= insts.len() as isize - 1 || p < 0 {
+    return None;
+  }
+
+  let pointer: u8 = p as u8;
+
+  if been_there.contains(&pointer) {
+    Some(acc)
+  } else {
+    let been_there = [been_there, vec![pointer]].concat();
+    acc_just_before_limbo(been_there, a, p, insts)
+  }
+}
+
+#[derive(Debug, Copy, Clone)]
+enum Op {
+  Nop,
+  Acc,
+  Jmp,
+}
+
+#[derive(Debug)]
+struct Instruction {
+  op: Op,
+  operand: i16,
+}
+
+impl Instruction {
+  fn values(&self) -> (Op, i16) {
+    (self.op, self.operand)
+  }
+}
+
+fn tokenize(v: Vec<String>) -> Vec<Instruction> {
+  v.iter().map(|l| line_to_token(l)).collect()
+}
+
+fn line_to_token(l: &str) -> Instruction {
+  let tokens: Vec<&str> = l.split(' ').collect(); // assume it's always one spacebar for now
+
+  let op = tokens[0];
+  let operand = tokens[1];
+
+  let sign = operand.chars().next().unwrap();
+
+  let number: String = operand.chars().skip(1).collect();
+  let number = number.parse::<i16>().unwrap();
+
+  let operand = match sign {
+    '+' => number,
+    _ => number * -1, // assuming it's always '-'
+  };
+
+  let op = match op {
+    "acc" => Op::Acc,
+    "jmp" => Op::Jmp,
+    _ => Op::Nop, // assuming it is always "nop"
+  };
+
+  Instruction { op, operand }
+}
+
+fn dummy() -> Vec<String> {
+  vec![
+    "nop +0", "acc +1", "jmp +4", "acc +3", "jmp -3", "acc -99", "acc +1", "jmp -4", "acc +6",
+  ]
+  .iter()
+  .map(|&s| s.into())
+  .collect()
+}
