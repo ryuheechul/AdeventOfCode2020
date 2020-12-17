921650331d89acc5841ed7570284820d30b32d1a
Heechul Ryu <ryuhcii@gmail.com>
2020-12-17 22:14:09 +0000

    [Day 2] add part 1

---
+++ day2/src/main.rs
@@ -0,0 +1,39 @@
+mod password;
+
+use password::PasswordValidation;
+
+fn main() -> Result<(), ()> {
+  let validations = parse_validation_from_file("./input.txt")?;
+
+  let count = validations.iter().filter(|v| v.is_valid()).count();
+
+  println!("the number of valid password from the input.txt is {}", count);
+
+  Ok(())
+}
+
+use std::fs::File;
+use std::io::{self, BufRead};
+use std::path::Path;
+
+// copied and pasted from https://doc.rust-lang.org/rust-by-example/std_misc/file/read_lines.html
+fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
+where
+  P: AsRef<Path>,
+{
+  let file = File::open(filename)?;
+  Ok(io::BufReader::new(file).lines())
+}
+
+fn parse_validation(v: &[String]) -> Vec<PasswordValidation> {
+  v.iter().map(|l| PasswordValidation::from(&l[..])).collect()
+}
+
+fn parse_validation_from_file(filename: &str) -> Result<Vec<PasswordValidation>, ()> {
+  read_lines(filename)
+    .map(|lines| {
+      let lines: Vec<String> = lines.map(|l| l.unwrap()).collect();
+      parse_validation(&lines)
+    })
+    .or(Err(()))
+}
---
+++ day2/src/password.rs
@@ -0,0 +1,123 @@
+#[derive(Debug, PartialEq)]
+struct PasswordPolicy {
+  min: u8,
+  max: u8,
+  charactor: char,
+}
+
+#[derive(Debug, PartialEq)]
+pub struct PasswordValidation {
+  policy: PasswordPolicy,
+  pw: String,
+}
+
+use std::convert::From;
+
+impl PasswordPolicy {
+  fn validate_for(&self, pw: &str) -> bool {
+    let count = pw.chars().filter(|&c| c == self.charactor).count();
+
+    let min: usize = self.min as usize;
+    let max: usize = self.max as usize;
+
+    min <= count && count <= max
+  }
+}
+
+impl From<&str> for PasswordPolicy {
+  fn from(policy_text: &str) -> Self {
+    let tokens: Vec<&str> = policy_text.split(' ').collect(); // assume it's always one spacebar for now
+
+    let range = tokens[0];
+    let charactor = tokens[1].chars().next().unwrap();
+
+    let tokens: Vec<&str> = range.split('-').collect(); // assume it's always one spacebar for now
+
+    let min = tokens[0];
+    let max = tokens[1];
+    let min: u8 = min.parse().unwrap();
+    let max: u8 = max.parse().unwrap();
+
+    PasswordPolicy {
+      min,
+      max,
+      charactor,
+    }
+  }
+}
+impl From<&str> for PasswordValidation {
+  fn from(l: &str) -> Self {
+    let tokens: Vec<&str> = l.split(':').collect(); // assume it's always one spacebar for now
+
+    let p_token = tokens[0];
+    let pw: String = tokens[1].trim().into();
+    let policy = PasswordPolicy::from(p_token);
+
+    PasswordValidation { policy, pw }
+  }
+}
+
+impl PasswordValidation {
+  pub fn is_valid(&self) -> bool {
+    self.policy.validate_for(&self.pw)
+  }
+}
+
+#[cfg(test)]
+mod tests {
+  use super::*;
+
+  // 1-3 a: abcde
+  #[test]
+  fn first() {
+    let policy = PasswordPolicy {
+      min: 1,
+      max: 3,
+      charactor: 'a',
+    };
+    let validation = PasswordValidation {
+      policy,
+      pw: "abcde".into(),
+    };
+    let from = PasswordValidation::from("1-3 a: abcde");
+
+    assert_eq!(from, validation);
+    assert!(from.is_valid());
+  }
+
+  // 1-3 b: cdefg
+  #[test]
+  fn second() {
+    let policy = PasswordPolicy {
+      min: 1,
+      max: 3,
+      charactor: 'b',
+    };
+    let validation = PasswordValidation {
+      policy,
+      pw: "cdefg".into(),
+    };
+    let from = PasswordValidation::from("1-3 b: cdefg");
+
+    assert_eq!(from, validation);
+    assert!(!from.is_valid());
+  }
+
+  // 2-9 c: ccccccccc
+  #[test]
+  fn third() {
+    let policy = PasswordPolicy {
+      min: 2,
+      max: 9,
+      charactor: 'c',
+    };
+    let validation = PasswordValidation {
+      policy,
+      pw: "ccccccccc".into(),
+    };
+    let from = PasswordValidation::from("2-9 c: ccccccccc");
+
+    assert_eq!(from, validation);
+    assert!(from.is_valid());
+  }
+}
